53f8b1584e104b521b7fdda157ce384616a64576
Billy Wong <ocis_nicola@hotmail.com>
2017-01-15 12:58:37 -0800

    Added comments

--- Student.php
+++ Student.php
@@ -12,32 +12,45 @@
  * @author Billy
  */
 class Student {
-    
+    /*
+     * Constructor for a Student class.  Contains the first name, last name,
+     * email, and grades.
+     */
     function __construct(){
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
-    
+    /*
+     * Adds a key/value pair to the emails array, where the first
+     * parameter is the email type and the second parameter is
+     * the email address.
+     */
     function add_email($which, $address){
         $this->emails[$which] = $address;
     }
-    
+    /*
+     * Adds a grade score to the grades array for the Student.
+     */
     function add_grade($grade){
         $this->grades[] = $grade;
     }
-    
+    /*
+     * Calculates the average of the Student's grades.
+     */
     function average(){
         $total = 0;
         foreach ($this->grades as $value)
             $total += $value;
         return $total / count($this->grades);
     }
-    
+    /*
+     * Displays the name, grade average, and emails of the Student.
+     */
     function toString(){
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' (' . $this->average() . "\n";
+        $result .= ' (' . $this->average() . ")\n";
         foreach ($this->emails as $which => $what)
             $result .= $which . ': ' . $what . "\n";
         $result .= "\n";
